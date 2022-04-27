79bb3655199c23572357de4093f6f415d06cedc2
Maennel <sabine.maennel@liip.ch>
2022-04-27 09:43:59 +0200

    fix: add showcase_search to app routes

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import routes, counts, groups, organizations, showcases
+from app import routes, counts, groups, organizations, showcases, showcase_search
 
 app.run(debug=True)
