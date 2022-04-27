e0d03fad158954751a1a2ebd79c10767cc83e0b5
Maennel <sabine.maennel@liip.ch>
2022-04-27 09:59:39 +0200

    feat: turn query arguments into cypher parameters

--- queries/showcase_search.py
+++ queries/showcase_search.py
@@ -1,10 +1,16 @@
 def search(tx, args):
     groups = args.getlist('groups')
+    groups_cypher = []
+
+    for group in groups:
+        groups_cypher.append({('g') + str(groups.index(group)): group})
+    print(groups_cypher)
     tags = args.getlist('tags')
     showcase_types = args.getlist('showcase_type')
     print(groups)
     print(tags)
     print(showcase_types)
+
     q = """MATCH (s:Showcase)-[:HAS_GROUP]->(g1:Group), (s:Showcase)-[:HAS_GROUP]->(g2:Group), 
     (s:Showcase) - [: HAS_GROUP]->(g3:Group)
     WHERE g1.group_name = 'work' AND g2.group_name = 'education'
