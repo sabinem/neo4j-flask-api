672554d8168c21c4379239462a914eabaeca8e33
Maennel <sabine.maennel@liip.ch>
2022-05-02 07:18:54 +0200

    fix: adapt lucene query analyzer so that it works for all cases
    in case of the showcases the fq part had always more parts:
    so only the children of the lucene tree needed to be analyzed
    
    for datasets also the tree itself might be an item that contains a
    facet

--- app/dataset_search.py
+++ app/dataset_search.py
@@ -5,35 +5,119 @@ from queries import dataset_search as q_dataset_search
 from utils import request_helpers as request_h
 from .routes import get_db
 
+dataset_facets = ['groups', 'res_format', 'keywords_en', 'organization', 'political_level', 'res_rights']
+
 
 @app.route("/dataset-search")
 def dataset_search():
     db = get_db()
     limit = request.args.get('rows', 20)
     skip = request.args.get('start', 0)
-    facet_dict = request_h.analyze_fq(request.args.get('fq'))
+    facet_dict = request_h.analyze_fq(request.args.get('fq'), dataset_facets)
     query_term = request.args.get('q')
     dataset_ids = db.read_transaction(
         q_dataset_search.dataset_search,
         facet_dict,
         query_term,
     )
-    #print("--------- first step")
-    #print(dataset_ids)
+    datasets = db.read_transaction(
+        q_dataset_search.get_datasets,
+        dataset_ids,
+        limit,
+        skip)
+    return Response(json.dumps(
+        {
+            "help": "request.url",
+            "success": True,
+            "result": {
+                "count": len(dataset_ids),
+                "sort": "core desc, metadata_modified desc",
+                "facets": {
+                    "res_format": {},
+                    "political_level": {},
+                    "groups": {},
+                    "organizations": {},
+                    "keywords_en": {},
+                    "res_rights": {},
+                },
+                "results": datasets,
+                "search_facets": {
+                    "res_format": {
+                        "items": [],
+                        "title": "res_formats",
+                    },
+                    "keywords_en": {
+                        "items": [],
+                        "title": "keywords_en",
+                    },
+                    "groups": {
+                        "items": [],
+                        "title": "groups",
+                    },
+                    "organization": {
+                        "items": [],
+                        "title": "organization",
+                    },
+                    "political_level": {
+                        "items": [],
+                        "title": "political_level",
+                    },
+                    "res_rights": {
+                        "items": [],
+                        "title": "groups",
+                    },
+                }
+            }
+        }), mimetype="application/json")
+
+
+def x():
+    db = get_db()
+    limit = request.args.get('rows', 20)
+    skip = request.args.get('start', 0)
+    facet_dict = request_h.analyze_fq(request.args.get('fq'), dataset_facets)
+    query_term = request.args.get('q')
+    dataset_ids = db.read_transaction(
+        q_dataset_search.dataset_search,
+        facet_dict,
+        query_term,
+    )
+    print(dataset_ids)
     if query_term:
         dataset_ids = db.read_transaction(
             q_dataset_search.get_query_search,
             query_term,
             dataset_ids,
         )
-    #print("--------- second step")
-    #print(dataset_ids)
     datasets = db.read_transaction(
         q_dataset_search.get_datasets,
         dataset_ids,
         limit,
         skip)
-    dataset_ids_page = [dataset.get('name') for dataset in datasets]
+    print(datasets)
+    return Response(json.dumps(
+        {
+            "help": "request.url",
+            "success": True,
+        }), mimetype="application/json")
+    print("--------- first step")
+    print(dataset_ids[0])
+    if query_term:
+        dataset_ids = db.read_transaction(
+            q_dataset_search.get_query_search,
+            query_term,
+            dataset_ids,
+        )
+    print("--------- second step")
+    dataset_ids = dataset_ids[:1]
+    print(dataset_ids)
+    datasets = db.read_transaction(
+        q_dataset_search.get_datasets,
+        dataset_ids,
+        limit,
+        skip)
+    print(datasets)
+    dataset_ids_page = [dataset.get('dataset_identifier') for dataset in datasets]
     groups_search_facets, groups_facets = db.read_transaction(
         q_dataset_search.get_groups_facets,
         dataset_ids
--- queries/dataset_search.py
+++ queries/dataset_search.py
@@ -16,78 +16,74 @@ def dataset_search(tx, facet_dict, query_term):
     where_clause = q_helpers.get_facet_where_clause(facets)
     if where_clause:
         q += where_clause
-    return_clause = "RETURN d.dataset_name as id"
+    return_clause = "RETURN d.dataset_identifier as id"
     q += return_clause
+    print(q)
     result = tx.run(q)
     return result.value('id')
 
 
-def get_query_search(tx, term, showcase_ids):
-    q = f"""CALL db.index.fulltext.queryNodes('showcase_de', '"{term}"') YIELD node, score"""
+def get_query_search(tx, term, dataset_ids):
+    q = f"""CALL db.index.fulltext.queryNodes('dataset_de', '"{term}"') YIELD node, score"""
     where_clause = q_helpers.get_ids_match_clause(
         match_clause="",
-        where_property='showcase_name',
+        where_property='dataset_identifier',
         match_node='node',
-        ids=showcase_ids
+        ids=dataset_ids
     )
-    return_clause = "RETURN node.showcase_name as id, score as score"
+    return_clause = "RETURN node.dataset_identifier as id, score as score"
     q += where_clause + return_clause
     result = tx.run(q)
     return result.value('id')
 
 
-def get_datasets(tx, showcase_ids, limit, skip):
+def get_datasets(tx, dataset_ids, limit, skip):
     match_clause = q_helpers.get_ids_match_clause(
-        match_clause="MATCH (s:Showcase) ",
-        where_property='showcase_name',
-        match_node='s',
-        ids=showcase_ids)
-    return_clause = "RETURN s "
-    pagination_clause = f"ORDER BY s.showcase_name Skip {skip} LIMIT {limit}"
+        match_clause="MATCH (o:Organization)<-[:BELONGS_TO]-(d:Dataset)-[:HAS_THEME]->(g:Group)",
+        where_property='dataset_identifier',
+        match_node='d',
+        ids=dataset_ids)
+    return_clause = "RETURN d, o, g "
+    pagination_clause = f"ORDER BY d.dataset_identifier Skip {skip} LIMIT {limit}"
     q = match_clause + return_clause + pagination_clause
     result = tx.run(q)
-    showcases = []
+    result.peek()
+    datasets = []
     for record in result:
-        showcase_dict = {}
-        for k, v in record['s'].items():
-            if k == 'showcase_name':
-                showcase_dict['name'] = v
-            showcase_dict[k] = v
-        showcases.append(showcase_dict)
-    return showcases
+        dataset_dict = {}
+        title_dict = {}
+        description_dict = {}
+        for k, v in record['d'].items():
+            if k.startswith('title_'):
+                title_dict[k.replace('title_', '')] = v
+            elif k.startswith('description_'):
+                description_dict[k.replace('description_', '')] = v
+            else:
+                dataset_dict[k] = v
+        dataset_dict['title'] = title_dict
+        dataset_dict['description'] = description_dict
+        title_dict = {}
+        description_dict = {}
+        for k, v in record['d'].items():
+            if k.startswith('title_'):
+                title_dict[k.replace('title_', '')] = v
+            elif k.startswith('description_'):
+                description_dict[k.replace('description_', '')] = v
+            else:
+                dataset_dict[k] = v
+        dataset_dict['title'] = title_dict
+        dataset_dict['description'] = description_dict
+        datasets.append(dataset_dict)
+    return datasets
 
 
-def get_groups_facets(tx,showcase_ids):
+def get_groups_facets(tx,dataset_ids):
     match_clause = q_helpers.get_ids_match_clause(
-        match_clause="MATCH (g:Group)<-[:HAS_GROUP]-(s:Showcase) ",
-        where_property='showcase_name',
-        match_node='s',
-        ids=showcase_ids)
+        match_clause="MATCH (g:Group)<-[:HAS_THEME]-(d:Dataset) ",
+        where_property='dataset_identifier',
+        match_node='d',
+        ids=dataset_ids)
     return_clause = "RETURN g, count(g) as count_g"
     q = match_clause + return_clause
     result = tx.run(q)
     return r_helpers.format_groups_facet_result(result)
-
-
-def get_showcase_type_facets(tx, showcase_ids):
-    match_clause = q_helpers.get_ids_match_clause(
-        match_clause="MATCH (a:Applicationtype)<-[r:HAS_APPLICATION_TYPE]-(s:Showcase) ",
-        where_property='showcase_name',
-        match_node='s',
-        ids=showcase_ids)
-    return_clause = "RETURN a, count(a) as count_a"
-    q = match_clause + return_clause
-    result = tx.run(q)
-    return r_helpers.format_facet_result(result, 'a', 'count_a', 'application_type_name')
-
-
-def get_tags_facets(tx, showcase_ids):
-    match_clause = q_helpers.get_ids_match_clause(
-        match_clause="MATCH (t:Tag)<-[r:HAS_TAG]-(s:Showcase) ",
-        where_property='showcase_name',
-        match_node='s',
-        ids=showcase_ids)
-    return_clause = "RETURN t, count(t) as count_t"
-    q = match_clause + return_clause
-    result = tx.run(q)
-    return r_helpers.format_facet_result(result, 't', 'count_t', 'tag_name')
