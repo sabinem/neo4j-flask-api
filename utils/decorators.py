b41dd16b92026321e779c693abf1e7e6cac1850d
sabinem <sabine.maennel@liip.ch>
2022-05-13 11:05:33 +0200

    refactor: improve query decorator

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,15 +1,32 @@
+import re
+import inspect
 from datetime import datetime
+from neo4j.work.transaction import Transaction
 from utils.query_builder import QueryResult
 
 
 def log_query(decorated):
     def inner(*args, **kwargs):
+        print("\nQUERY ===========================================================================")
+        print("---- args")
+        print(inspect.signature(decorated))
+        for arg in args:
+            if type(arg) == Transaction:
+                pass
+            elif type(arg) == dict:
+                print(f"dictionary with # of keys: {len(arg.keys())}")
+            elif type(arg) == list:
+                print(f"{arg[:5]} ... (list truncated)")
+            else:
+                print(arg)
+        print("---- docs")
+        print(f"Purpose: {decorated.__doc__}")
         t_start = datetime.now()
         query_result = decorated(*args, **kwargs)
         t_finish = datetime.now()
-        print("---------------- Query ------------------------")
-        print(f"Purpose: {decorated.__doc__}")
-        print(f"Runtime: {t_finish - t_start}")
-        print(query_result.query)
+        print("---- performance (runtime)")
+        print(f"{t_finish - t_start}")
+        print("---- query")
+        print(re.sub('IN \[[\S]*\]', 'IN [...]', query_result.query))
         return query_result.result
     return inner
